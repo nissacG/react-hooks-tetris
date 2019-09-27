ad3ee263c7a09295ae461fe3db78f5e1cb0526a8
Gareth Cassin <nissacjg@gmail.com>
2019-09-27 09:03:41 +0100

    Create gameHelper.ts
    function to createStage which produces nested arrays which will be used to create the cells being filled by the tetrominos (tetris pieces)

---
+++ src/gameHelper.ts
@@ -0,0 +1,10 @@
+export const STAGE_WIDTH = 12
+export const STAGE_HEIGHT = 20
+
+export const createState = () => (
+  // create Array from an array, created from the STAGE_HEIGHT. 
+  Array.from(Array(STAGE_HEIGHT), () => ( // [[], [], []...]
+    // on each item in that array, create a new array from STAGE_WIDTH filled with array of [0, 'clear']
+    new Array(STAGE_WIDTH).fill([0, 'clear']) // result is [([([0, 'clear'] x12)] x20) ]
+  ))
+)
