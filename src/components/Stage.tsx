70d9fd6a50aabad22958f9747ec21feac22809c6
Gareth Cassin <nissacjg@gmail.com>
2019-09-29 18:24:10 +0100

    style stage (grid)

--- src/components/Stage.tsx
+++ src/components/Stage.tsx
@@ -6,8 +6,13 @@ interface StageProps {
   stage: [string, number][][]
 }
 
+interface Styles {
+  height: number
+  width: number
+}
+
 const Stage: FC<StageProps> = ({ stage }) => (
-  <StyledStage className='stage'>
+  <StyledStage className='stage' height={stage.length} width={stage[0].length} >
     {
       stage.map((row: any[]) => (
         row.map((cell: any[], index: number ) => (
@@ -18,8 +23,21 @@ const Stage: FC<StageProps> = ({ stage }) => (
   </StyledStage>
 )
 
-const StyledStage = styled.div`
-
+const StyledStage = styled.div<Styles>`
+  display: grid;
+  grid-template-rows: repeat(
+    ${props => props.height},
+    calc(25vw / ${props => props.width})
+  );
+  grid-template-columns: repeat(
+    ${props => props.width},
+    1fr
+  );
+  grid-gap: 1px;
+  border: 2px solid #333;
+  width: 100%;
+  max-width: 25vw;
+  background: #111;
 `
 
 export default Stage
