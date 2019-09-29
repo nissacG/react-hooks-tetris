6f0134dc1b48cf4348aa89b3fd6b01974a6d3c37
Gareth Cassin <nissacjg@gmail.com>
2019-09-29 17:32:13 +0100

    tetris styling

--- src/components/Tetris.tsx
+++ src/components/Tetris.tsx
@@ -5,24 +5,48 @@ import Display from './Display'
 import StartBtn from './StartBtn'
 
 import { createStage } from '../gameHelper'
+import bgImage from '../img/retro-bg.jpg'
 
 const Tetris = () => {
 
   return (
-    <StyledTetris>
-      <Stage stage={createStage()}/>
-      <aside>
-        <Display text='Score' />
-        <Display text='Rows' />
-        <Display text='Level' />
-        <StartBtn callback={() => console.log('clicked')} />
-      </aside>
+    <StyledTetris className='tetris-wrapper'>
+      <div className='tetris'>
+        <Stage stage={createStage()}/>
+        <aside>
+          <Display text='Score' />
+          <Display text='Rows' />
+          <Display text='Level' />
+          <StartBtn callback={() => console.log('clicked')} />
+        </aside>
+      </div>
     </StyledTetris>
   )
 }
 
 const StyledTetris = styled.div`
-  font-family: 'Arcade Classic'
+  width: 100vw;
+  height: 100vh;
+  background-image: url(${bgImage});
+  background-size: cover;
+  font-family: 'Arcade Classic';
+  overflow: hidden;
+
+  & .tetris {
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+    padding: 40px;
+    margin: 0 auto;
+    max-width: 900px;
+
+    & aside {
+      width: 100%;
+      max-width: 200px;
+      display: block;
+      padding: 0 20px;
+    }
+  }
 `
 
 export default Tetris
