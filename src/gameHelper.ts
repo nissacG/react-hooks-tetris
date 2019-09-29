3423c87f659ea64179358148c155d254ea669693
Gareth Cassin <nissacjg@gmail.com>
2019-09-29 16:17:11 +0100

    correct funtion name spelling

--- src/gameHelper.ts
+++ src/gameHelper.ts
@@ -1,7 +1,7 @@
 export const STAGE_WIDTH = 12
 export const STAGE_HEIGHT = 20
 
-export const createState = () => (
+export const createStage = () => (
   // create Array from an array, created from the STAGE_HEIGHT. 
   Array.from(Array(STAGE_HEIGHT), () => ( // [[], [], []...]
     // on each item in that array, create a new array from STAGE_WIDTH filled with array of [0, 'clear']
