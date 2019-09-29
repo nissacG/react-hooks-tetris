4e46f78d96e166b2508a49f895766cc6588076cc
Gareth Cassin <nissacjg@gmail.com>
2019-09-29 18:23:55 +0100

    style start button

--- src/components/StartBtn.tsx
+++ src/components/StartBtn.tsx
@@ -7,12 +7,25 @@ interface StartBtnProps {
 
 const StartBtn: FC<StartBtnProps> = ({ callback }) => (
   <StyledStartBtn className='start-btn' onClick={callback}>
-    StartBtn
+    Start Game
   </StyledStartBtn>
 )
 
 const StyledStartBtn = styled.div`
-
+  display: flex;
+  justify-content: center;
+  box-sizing: border-box;
+  margin: 0 0 20px 0;
+  padding: 20px;
+  min-height: 30px;
+  width: 100%;
+  border-radius: 10px;
+  border: none;
+  background: #333;
+  color: #fff;
+  font-size: 1.2rem;
+  outline: none;
+  cursor: pointer;
 `
 
 export default StartBtn
