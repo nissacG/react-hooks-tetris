83d2f04bdb28ce1be9678229f72f07a00b3d6a53
Gareth Cassin <nissacjg@gmail.com>
2019-09-29 16:18:06 +0100

    update stage typing and map over stage prop to produce all cells
    need to style still

--- src/components/Stage.tsx
+++ src/components/Stage.tsx
@@ -3,12 +3,18 @@ import styled from 'styled-components'
 import Cell from './Cell'
 
 interface StageProps {
-  stage: string[]
+  stage: [string, number][][]
 }
 
 const Stage: FC<StageProps> = ({ stage }) => (
   <StyledStage>
-    Stage
+    {
+      stage.map((row: any[]) => (
+        row.map((cell: any[], index: number ) => (
+          <Cell key={index} type={cell[0]} />
+        ))
+      ))
+    }
   </StyledStage>
 )
 
