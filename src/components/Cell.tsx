fb8cff5313a2f0d50f1369267d108c668799f0f8
Gareth Cassin <nissacjg@gmail.com>
2019-09-29 17:32:01 +0100

    style cells
    using 'L' as example that its working

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,12 +1,29 @@
 import React, { FC } from 'react'
+import styled from 'styled-components'
+import { tetrominos } from '../tetrominos'
 
 interface CellProps {
-  type: number
+  type: string | number
+}
+
+interface Styled {
+  type: string | number
+  color: string
 }
 
 const Cell: FC<CellProps> = ({ type }) => (
-  <div>
-    {type}
-  </div>
+  <StyledCell type={'L'} color={tetrominos['L'].color} />
 )
+
+
+
+const StyledCell = styled.div<Styled>`
+  width: auto;
+  background: rgba(${props => props.color}, 0.8);
+  border: ${props => (props.type === 0 ? '0px' : '4px solid')};
+  border-bottom-color: rgba(${props => props.color}, 0.1);
+  border-right-color: rgba(${props => props.color}, 1);
+  border-left-color: rgba(${props => props.color}, 1); 
+  border-top-color: rgba(${props => props.color}, 0.3);
+`
 export default Cell
